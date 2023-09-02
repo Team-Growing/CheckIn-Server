f77062668d720042ad408e1c05fd06158def08eb
이지민 <wlals060330@gmail.com>
2023-09-02 20:58:14 +0900

    :sparkles: :: add batch config

--- src/main/java/dgsw/pioneers/checkIn/global/config/batch/BatchConfig.java
+++ src/main/java/dgsw/pioneers/checkIn/global/config/batch/BatchConfig.java
@@ -1,7 +1,13 @@
 package dgsw.pioneers.checkIn.global.config.batch;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dgsw.pioneers.checkIn.global.batch.step.dto.AttendanceJobDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
+import org.springframework.batch.core.repository.ExecutionContextSerializer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.dao.Jackson2ExecutionContextStringSerializer;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
@@ -11,12 +17,12 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
-//@Configuration
-//@RequiredArgsConstructor
-//public class BatchConfig {
-//
-//    private final ObjectMapper objectMapper;
-//
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    private final ObjectMapper objectMapper;
+
 //    @Bean
 //    public JobRepository createJobRepository(DataSource dataSource, PlatformTransactionManager transactionManager) throws Exception {
 //
@@ -32,4 +38,41 @@ import javax.sql.DataSource;
 //
 //        return jobRepositoryFactoryBean.getObject();
 //    }
-//}
+
+    @Bean
+    public ExecutionContextSerializer customSerializer() {
+        Jackson2ExecutionContextStringSerializer defaultSerializer = new Jackson2ExecutionContextStringSerializer(AttendanceJobDto.class.getName());
+        defaultSerializer.setObjectMapper(objectMapper);
+
+        return defaultSerializer;
+//        Jackson2ExecutionContextStringSerializer(AttendanceJobDto.class.getName())
+    }
+
+    @Bean
+    public BatchConfigurer myBatchConfigurer(DataSource dataSource,
+                                             ExecutionContextSerializer executionContextSerializer,
+                                             PlatformTransactionManager transactionManager) {
+        return new DefaultBatchConfigurer(dataSource) {
+            @Override
+            protected JobExplorer createJobExplorer() throws Exception {
+                JobExplorerFactoryBean jobExplorerFactoryBean = new JobExplorerFactoryBean();
+                jobExplorerFactoryBean.setDataSource(dataSource);
+                jobExplorerFactoryBean
+                        .setSerializer(executionContextSerializer);
+                jobExplorerFactoryBean.afterPropertiesSet();
+                return jobExplorerFactoryBean.getObject();
+            }
+
+            @Override
+            protected JobRepository createJobRepository() throws Exception {
+                JobRepositoryFactoryBean jobRepositoryFactoryBean = new JobRepositoryFactoryBean();
+                jobRepositoryFactoryBean.setDataSource(dataSource);
+                jobRepositoryFactoryBean
+                        .setSerializer(executionContextSerializer);
+                jobRepositoryFactoryBean.setTransactionManager(transactionManager);
+                jobRepositoryFactoryBean.afterPropertiesSet();
+                return jobRepositoryFactoryBean.getObject();
+            }
+        };
+    }
+}
--- src/main/java/dgsw/pioneers/checkIn/global/config/jackson/JacksonConfig.java
+++ src/main/java/dgsw/pioneers/checkIn/global/config/jackson/JacksonConfig.java
@@ -1,26 +1,11 @@
 package dgsw.pioneers.checkIn.global.config.jackson;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import dgsw.pioneers.checkIn.global.batch.step.dto.AttendanceJobDto;
-import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
-import org.springframework.batch.core.repository.ExecutionContextSerializer;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.dao.Jackson2ExecutionContextStringSerializer;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class JacksonConfig {
@@ -34,38 +19,5 @@ public class JacksonConfig {
 //        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
 
         return objectMapper;
-    }
-
-    @Bean
-    public ExecutionContextSerializer customSerializer() {
-        return new Jackson2ExecutionContextStringSerializer(AttendanceJobDto.class.getName());
-    }
-
-    @Bean
-    public BatchConfigurer myBatchConfigurer(DataSource dataSource,
-                                             ExecutionContextSerializer executionContextSerializer,
-                                             PlatformTransactionManager transactionManager) {
-        return new DefaultBatchConfigurer(dataSource) {
-            @Override
-            protected JobExplorer createJobExplorer() throws Exception {
-                JobExplorerFactoryBean jobExplorerFactoryBean = new JobExplorerFactoryBean();
-                jobExplorerFactoryBean.setDataSource(dataSource);
-                jobExplorerFactoryBean
-                        .setSerializer(executionContextSerializer);
-                jobExplorerFactoryBean.afterPropertiesSet();
-                return jobExplorerFactoryBean.getObject();
-            }
-
-            @Override
-            protected JobRepository createJobRepository() throws Exception {
-                JobRepositoryFactoryBean jobRepositoryFactoryBean = new JobRepositoryFactoryBean();
-                jobRepositoryFactoryBean.setDataSource(dataSource);
-                jobRepositoryFactoryBean
-                        .setSerializer(executionContextSerializer);
-                jobRepositoryFactoryBean.setTransactionManager(transactionManager);
-                jobRepositoryFactoryBean.afterPropertiesSet();
-                return jobRepositoryFactoryBean.getObject();
-            }
-        };
     }
 }
