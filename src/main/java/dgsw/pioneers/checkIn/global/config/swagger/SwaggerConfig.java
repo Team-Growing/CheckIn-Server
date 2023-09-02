d1bae067a72bae68714b9f537fedbf3310cf921b
이지민 <wlals060330@gmail.com>
2023-09-02 18:48:09 +0900

    :memo: :: add swagger authorization

--- src/main/java/dgsw/pioneers/checkIn/global/config/swagger/SwaggerConfig.java
+++ src/main/java/dgsw/pioneers/checkIn/global/config/swagger/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package dgsw.pioneers.checkIn.global.config.swagger;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 @Configuration
 public class SwaggerConfig {
 
