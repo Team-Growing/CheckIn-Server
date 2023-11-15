52e100a0e8237ffadb65871c8867d8a162a9aa18
이지민 <wlals060330@gmail.com>
2023-11-15 16:42:12 +0900

    :memo: :: swagger config fix

--- src/main/java/dgsw/pioneers/checkIn/global/config/swagger/SwaggerConfig.java
+++ src/main/java/dgsw/pioneers/checkIn/global/config/swagger/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +29,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(new Info()
                         .title("CheckIn")
                         .description("CheckIn V1 Api Docs")
